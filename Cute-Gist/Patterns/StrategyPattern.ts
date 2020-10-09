5fd8c2dea622b17b24dee36ae034ffeaa2e2d501
wangpingan <pingan8787@qq.com>
2020-10-09 08:13:39 +0800

    feat: 2020.10.09

--- Cute-Gist/Patterns/StrategyPattern.ts
+++ Cute-Gist/Patterns/StrategyPattern.ts
@@ -1,3 +1,7 @@
+/**
+ * DEMO1
+ */
+
 interface Strategy<T> {
     execute(data: T): T
 }
@@ -34,3 +38,57 @@ console.log(context.doSomething(10))
 
 context.setStrategy(new Leo2ConcreteStrategy());
 console.log(context.doSomething(100))
+
+/**
+ * DEMO2
+ */
+interface Strategy {
+    authenticate(...args: any): any;
+  }
+  
+  class Authenticator {
+    strategy: any;
+    constructor() {
+      this.strategy = null;
+    }
+  
+    setStrategy(strategy: any) {
+      this.strategy = strategy;
+    }
+  
+    authenticate(...args: any) {
+      if (!this.strategy) {
+        console.log('尚未设置认证策略');
+        return;
+      }
+      return this.strategy.authenticate(...args);
+    }
+  }
+  
+  class WechatStrategy implements Strategy {
+    authenticate(wechatToken: string) {
+      if (wechatToken !== '123') {
+        console.log('无效的微信用户');
+        return;
+      }
+      console.log('微信认证成功');
+    }
+  }
+  
+  class LocalStrategy implements Strategy {
+    authenticate(username: string, password: string) {
+      if (username !== 'abao' && password !== '123') {
+        console.log('账号或密码错误');
+        return;
+      }
+      console.log('账号和密码认证成功');
+    }
+  }
+  
+  const auth = new Authenticator();
+  
+  auth.setStrategy(new WechatStrategy());
+  auth.authenticate('123456');
+  
+  auth.setStrategy(new LocalStrategy());
+  auth.authenticate('abao', '123');
