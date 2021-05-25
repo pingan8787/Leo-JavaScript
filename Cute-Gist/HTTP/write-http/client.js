1052dd783390865252a0b35e1f4b0b5ba40528a6
wangpingan <pingan8787@qq.com>
2021-05-25 23:20:21 +0800

    2021.05.25

--- Cute-Gist/HTTP/write-http/client.js
+++ Cute-Gist/HTTP/write-http/client.js
@@ -1,9 +1,10 @@
 const net = require('net')
+const testHeader = `POST / HTTP/1.1\r\nContent-Type: application/json\r\nUser-Agent: PostmanRuntime/7.17.1\r\nAccept: */*\r\nCache-Control: no-cache\r\nPostman-Token: 5041de72-27c3-44c6-99e8-c04c306b11ef\r\nHost: localhost:8888\r\nAccept-Encoding: gzip, deflate\r\nContent-Length: 19\r\nConnection: keep-alive\r\n\r\n{name: "jack"}`;
+
 const client = net.createConnection({ port: 8080 }, () => {
     // 'connect' 监听器
     console.log('已连接到服务器');
-    client.write(`POST / HTTP/1.1\r\nContent-Type: application/json\r\nUser-Agent: PostmanRuntime/7.17.1\r\nAccept: */*\r\nCache-Control: no-cache\r\nPostman-Token: 5041de72-27c3-44c6-99e8-c04c306b11ef\r\nHost: localhost:8888\r\nAccept-Encoding: gzip, deflate\r\nContent-Length: 19\r\nConnection: keep-alive\r\n\r\n{name: "jack"}`
-    );
+    client.write(testHeader);
 });
 //客户端收到服务端执行的事件
 client.on('data', data => {
