e934b56c418464b1a9ad65d7ca5b129409c27784
wangpingan <pingan8787@qq.com>
2021-06-17 00:11:47 +0800

    2021.06.17

--- Cute-Gist/Web/upload-file-demo/client/utils/index.js
+++ Cute-Gist/Web/upload-file-demo/client/utils/index.js
@@ -1,4 +1,31 @@
-const readBuffer = (file, start = 0, end = 2) => {
+const createElement = (tag, attr) => {
+    if(!tag) return;
+    const elem = document.createElement(tag);
+    // 还需要判断是个可迭代对象
+    if(attr) Object.assign(elem, attr);
+    return elem;
+}
+
+const createPreviewItem = async (container, file) => {
+    if(!container || !file) return;
+    const src = await readDataURL(file);
+    const item = createElement("div", {className: "preview-item"});
+    const title = createElement("div", {
+        className: "preview-title", 
+        innerText: "文件名：" + file.name
+    });
+    const size = createElement("div", {
+        className: "preview-size", 
+        innerText: "文件大小：" + file.size
+    });
+    const img = createElement("img", {src, className: "preview-image"});
+    item.append(img);
+    item.append(title);
+    item.append(size);
+    container.append(item);
+}
+
+const readDataURL = file => {
     return new Promise((resolve, reject) => {
         if(!file) resolve();
         const reader = new FileReader();
@@ -10,14 +37,18 @@ const readBuffer = (file, start = 0, end = 2) => {
 
 const isImage = async file => {
     if(!file) return;
-    const base64 = await readBuffer(file);
+    const base64 = await readDataURL(file);
     return base64 && base64.slice(0, 10) === "data:image";
 }
 
-const uploadProgress= async (progressEvent) => {
-    const divNumber = document.querySelector("#progress-number");
-    const divTime = document.querySelector("#progress-time");
+const uploadProgress = async (progressEvent) => {
+    const divNumber = document.querySelector("#progressNumber");
+    const divTime = document.querySelector("#progressTime");
     let complete = (progressEvent.loaded / progressEvent.total * 100 | 0)
     divNumber.innerText = complete;
     divTime.innerText = Math.ceil(progressEvent.timeStamp);
+}
+
+const downloadProgress = async (progressEvent) => {
+
 }
