535aabdf1c7999717d7191ebc99a5c14639f0b0c
wangpingan <pingan8787@qq.com>
2021-07-23 07:09:17 +0800

    使用 loader-utils

--- Cute-Gist/Webpack/手写 txt loader/txt-loader/txt-replace-loader.js
+++ Cute-Gist/Webpack/手写 txt loader/txt-loader/txt-replace-loader.js
@@ -1,11 +1,15 @@
+const { getOptions } = require("loader-utils");
+
 const word = ["刀","枪","棍"];
 
-module.exports = source => {
+module.exports = function(source, map, meta) {
     console.log('[txt-replace-loader][normal loader]', source)
-    word.forEach(w => source = source.replace(w, '**'))
+    const { tags = '**' } = getOptions(this);
+    word.forEach(w => source = source.replace(w, tags))
     source = '====[txt-replace-loader][start]====' + source + '====[txt-replace-loader][end]====';
     return source;
 }
+
 module.exports.pitch = function (remainingRequest, precedingRequest, data) {
     // console.log('[txt-replace-loader][pitch loader]', remainingRequest, precedingRequest, data)
 };
