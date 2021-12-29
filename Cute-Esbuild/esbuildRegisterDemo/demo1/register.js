fdad8614afc283eac6da2ffa9463d3e28338d6c0
wangpingan <pingan8787@qq.com>
2021-12-29 08:15:06 +0800

    feat: add esbuild-register

---
+++ Cute-Esbuild/esbuildRegisterDemo/demo1/register.js
@@ -0,0 +1,19 @@
+const { register } = require('esbuild-register/dist/node')
+// interface RegisterOptions extends TransformOptions {
+//     extensions?: EXTENSIONS[]
+//     /**
+//      * Auto-ignore node_modules. Independent of any matcher.
+//      * @default true
+//      */
+//     hookIgnoreNodeModules?: boolean
+//     /**
+//      * A matcher function, will be called with path to a file. Should return truthy if the file should be hooked, falsy otherwise.
+//      */
+//     hookMatcher?(fileName: string): boolean
+//   }
+const { unregister } = register({
+    
+})
+
+// Unregister the require hook if you don't need it anymore
+// unregister()
