259e59e914baaeeeb45e71172a2b8d6bbee692d1
wangpingan <pingan8787@qq.com>
2022-01-04 08:15:40 +0800

    feat: 2022.01.04

--- Cute-Gist/Promise/API/Promise.all.js
+++ Cute-Gist/Promise/API/Promise.all.js
@@ -1,3 +1,4 @@
+// 普通使用
 const p1 = new Promise((resolve, reject) => {
     resolve([1,2,3]);
 })
@@ -17,4 +18,20 @@ p.then(data => {
     console.log('[data]',data)
 }).catch(err => {
     console.log('[err]',err)
+})
+
+// 参数的 Promise 自己定义 catch，则 Promise.all 的 catch 方法不执行
+
+const p4 = new Promise((resolve, reject) => {
+    reject([7,8,9]);
+}).catch(err => {
+    console.log('[p4 err]',err)
+})
+
+const po1 = Promise.all([p1, p2, p3]);
+
+po1.then(data => {
+    console.log('[data]',data)
+}).catch(err => {
+    console.log('[po1 err]',err)
 })
