2196f0df1c1f0a03c4941e8a585ab7bbb0c3b963
wangpingan <pingan8787@qq.com>
2022-06-20 07:50:10 +0800

    feat: add middler demo

---
+++ Cute-Demo/21.middler-dome.js
@@ -0,0 +1,31 @@
+const c1 = next => {
+    console.log('[c1-1]');
+    next();
+    console.log('[c1-2]');
+}
+
+const c2 = next => {
+    console.log('[c2-1]');
+    next();
+    console.log('[c2-2]');
+}
+
+const runCallbacks = cbs => {
+    if(Array.isArray(cbs)){
+        let idx = 0;
+        const next = () => {
+            const cb = cbs[idx++];
+            typeof cb === 'function' && cb.call(cb, next);
+        }
+        next();
+    }
+}
+
+runCallbacks([c1, c2]);
+/**
+ 输出结果：
+ [c1-1]
+ [c1-2]
+ [c2-1]
+ [c2-2]
+ */
---
+++ Cute-Demo/dist/21.middler-dome.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var c1 = function c1(next) {
+  console.log('[c1-1]');
+  next();
+  console.log('[c1-2]');
+};
+
+var c2 = function c2(next) {
+  console.log('[c2-1]');
+  next();
+  console.log('[c2-2]');
+};
+
+var runCallbacks = function runCallbacks(cbs) {
+  if (Array.isArray(cbs)) {
+    var idx = 0;
+
+    var next = function next() {
+      var cb = cbs[idx++];
+      typeof cb === 'function' && cb.call(cb, next);
+    };
+
+    next();
+  }
+};
+
+runCallbacks([c1, c2]);
+/**
+ 输出结果：
+ [c1-1]
+ [c1-2]
+ [c2-1]
+ [c2-2]
+ */
